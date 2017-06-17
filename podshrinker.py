1d68db8848fcd01f238d2186dcee45b513b9bf84
Ceri Storey <cez@necrofish.org.uk>
2017-06-18 00:58:30 +0100

    Handle running behind reverse proxy.

--- podshrinker.py
+++ podshrinker.py
@@ -277,5 +277,7 @@ def transcode_command(orig, bitrate=32):
 if __name__ == '__main__':
   from waitress import serve
   import os
+  from werkzeug.contrib.fixers import ProxyFix
+  app.wsgi_app = ProxyFix(app.wsgi_app)
   port = int(os.environ.get('PORT', 5000))
   serve(app, port=port)
