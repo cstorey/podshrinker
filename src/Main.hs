ec4473825e66b00daba5c94ac4ab86eb3694d8a3
Ceri Storey <cez@necrofish.org.uk>
2018-01-24 22:43:29 +0000

    Display URI if we have it.

--- src/Main.hs
+++ src/Main.hs
@@ -5,6 +5,9 @@ import           Control.Applicative
 import           Snap.Core
 import           Snap.Util.FileServe
 import           Snap.Http.Server
+import           Data.ByteString.Lazy as L
+import           Data.Text.Encoding (decodeUtf8)
+import           Data.Text
 import           Text.Blaze.Html5 ((!))
 import qualified Text.Blaze.Html5 as H
 import qualified Text.Blaze.Html5.Attributes as A
@@ -28,20 +31,30 @@ main = quickHttpServe site
 
 indexPage :: Snap()
 indexPage = do
+    uri <-  getParam "uri"
+    let uri2 = fmap decodeUtf8 uri
+
     modifyResponse $ addHeader "Content-Type" "text/html; charset=UTF-8"
-    writeLBS $ renderHtml content
+    writeLBS $ response uri2
   where
-    content = do
+    content uri = do
       H.docType
       H.html $ do
         H.head $ H.title "Internal Server Error"
         H.body $ do
           H.h1 "Thingy"
-          H.form ! A.formaction action $ do
+          H.form $ do
             H.label ! A.for "uri" $ "URI:"
             H.input ! A.name "uri" ! A.type_ "text"
             H.button ! A.type_ "submit" $ "Go!"
-    action = "/some/thing"
+          H.p $ maybe noUri hasUri uri
+
+    noUri = H.span "No url"
+    hasUri uri = H.p $ do
+        H.span "URI: "
+        H.toHtml uri
+    response :: Maybe Text -> L.ByteString
+    response uri = renderHtml $ content uri
 
 
 site :: Snap ()
