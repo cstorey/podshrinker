e23da29130525b1b957a9d9d55ee257d0a81a55c
Ceri Storey <ceri.storey@oliverwyman.com>
2018-01-25 17:37:02 +0000

    Build some kind of URI.

--- src/Main.hs
+++ src/Main.hs
@@ -5,18 +5,19 @@ import           Control.Applicative
 import           Snap.Core
 import           Snap.Util.FileServe
 import           Snap.Http.Server
-import           Data.ByteString.Lazy as L
 import           Data.ByteString as B
 import           Data.Text.Encoding (decodeUtf8)
-import           Data.Text
+import           Data.Text as T
 import           Text.Blaze.Html5 ((!))
 import qualified Text.Blaze.Html5 as H
 import qualified Text.Blaze.Html5.Attributes as A
 import           Text.Blaze.Html.Renderer.Utf8 (renderHtml)
-import           Crypto.MAC.HMAC (HMAC, hmac, hmacGetDigest)
+import           Crypto.MAC.HMAC (hmac, hmacGetDigest)
 import           Crypto.Hash.Algorithms (Blake2s_256)
 import           Crypto.Hash (Digest)
 import           Data.ByteArray.Encoding (convertToBase, Base(Base64URLUnpadded))
+import qualified Network.URI as U
+import           Data.List (intercalate)
 
 
 main :: IO ()
@@ -37,41 +38,48 @@ main = quickHttpServe site
 indexPage :: Snap()
 indexPage = do
     uri <-  getParam "uri"
+    rq <- getRequest
     modifyResponse $ addHeader "Content-Type" "text/html; charset=UTF-8"
-    writeLBS $ response uri
+    writeLBS $ response rq uri
   where
-    content uri = do
+    content rq uri = do
       H.docType
       H.html $ do
-        H.head $ H.title "Internal Server Error"
+        H.head $ H.title "Things"
         H.body $ do
           H.h1 "Thingy"
           H.form $ do
             H.label ! A.for "uri" $ "URI:"
             H.input ! A.name "uri" ! A.type_ "text"
             H.button ! A.type_ "submit" $ "Go!"
-          H.p $ maybe noUri hasUri uri
+          H.p $ maybe noUri (hasUri rq) uri
 
     noUri = H.span "No url"
-    hasUri uri = do
+    hasUri rq uri = do
       H.p $ do
         H.span "URI: "
         H.toHtml $ decodeUtf8 uri
       H.p $ do
         H.span "Mac: "
-        H.toHtml $ signUri uri
-    response uri = renderHtml $ content uri
+        H.toHtml $ signUri rq uri
+    response rq uri = renderHtml $ content rq uri
 
 
-signUri :: B.ByteString -> Text
-
-signUri uri = undigest
+signUri :: Request -> B.ByteString -> Text
+signUri rq uri = T.pack $ U.uriToString id feedUri ""
   where
     key = ("FIXME" :: B.ByteString)
     mac :: Digest Blake2s_256
     mac = hmacGetDigest $ hmac key uri
 
     undigest = decodeUtf8 $ convertToBase Base64URLUnpadded mac
+    baseUri :: U.URI
+    baseUri = U.URI "https" Nothing authority "/" ""
+    authority = "//" ++ (T.unpack $ decodeUtf8 $ rqHostName rq)
+    feedUri = U.URI "https" Nothing authority feedPath ""
+    feedPath = Data.List.intercalate "/" ["", "feed", macComp, uriComp]
+    macComp = U.escapeURIString U.isUnescapedInURIComponent $ T.unpack undigest
+    uriComp = U.escapeURIString U.isUnescapedInURIComponent $ T.unpack $ decodeUtf8 uri
 
 site :: Snap ()
 site =
